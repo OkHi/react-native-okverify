a41b425f9bb91eb6dfe6d651c24dd5b00d45a7bf
Julius Kiano <jgkiano@gmail.com>
2020-08-27 15:51:20 +0300

    initial java configuration

---
+++ android/src/main/java/com/okhireactnativeokverify/ReactNativeOkverifyModule.java
@@ -0,0 +1,26 @@
+package com.okhireactnativeokverify;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class ReactNativeOkverifyModule extends ReactContextBaseJavaModule {
+
+  public ReactNativeOkverifyModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @NonNull
+  @Override
+  public String getName() {
+    return "ReactNativeOkverify";
+  }
+
+  @ReactMethod
+  public void multiply(int a, int b, Promise promise) {
+    promise.resolve(a*b*2);
+  }
+}
