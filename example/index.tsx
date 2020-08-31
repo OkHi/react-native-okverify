4535889c8dd709bd55d96bb578d1f343dc768c83
Julius Kiano <jgkiano@gmail.com>
2020-08-31 08:06:29 +0300

    use notifications on init

--- example/index.tsx
+++ example/index.tsx
@@ -1,5 +1,14 @@
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
+import * as OkVerify from '@okhi/react-native-okverify';
+
+OkVerify.init({
+  title: 'Verification in progress',
+  text: 'Verification in progress',
+  channelDescription: 'OkHi verification status updates',
+  channelId: 'okhi',
+  channelName: 'OkHi Verification',
+});
 
 AppRegistry.registerComponent(appName, () => App);
