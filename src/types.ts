9104bb35bb3714710ad01126b9ab630537b5aeec
Julius Kiano <jgkiano@gmail.com>
2020-09-05 20:50:54 +0300

    moved imports for cleaner docs

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,15 @@
-export interface OkHiNotification {
-  title: string;
-  text: string;
-  channelId: string;
-  channelName: string;
-  channelDescription: string;
-  importance?: number;
-  icon?: number;
-}
+import type { OkHiNotification } from './index';
+
+export type OkVerifyType = {
+  init(notification: OkHiNotification | { [key: string]: any }): void;
+  start(configuration: {
+    branchId: string;
+    clientKey: string;
+    phone: string;
+    locationId: string;
+    lat: number;
+    lon: number;
+    mode: string;
+  }): Promise<string>;
+  stop(locationId: string): Promise<string>;
+};
