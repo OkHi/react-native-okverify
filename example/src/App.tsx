245fad9e2ef17e707d4ab9ed77af4312aca6dd3c
Julius Kiano <jgkiano@gmail.com>
2021-06-17 15:49:39 +0300

    removed unused files

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { StyleSheet, View, Button } from 'react-native';
-import OkHiLocationManager from '@okhi/react-native-okcollect';
-import {
-  canStartVerification,
-  startVerification,
-} from '@okhi/react-native-okverify';
+// import OkHiLocationManager from '@okhi/react-native-okcollect';
+// import {
+//   canStartVerification,
+//   startVerification,
+// } from '@okhi/react-native-okverify';
 
 // import secret from './secret.json';
 
@@ -13,42 +13,42 @@ export default function App() {
 
   console.log(launch);
 
-  const handleOnSuccess = async (response: any) => {
-    setLaunch(false);
-    try {
-      const isReady = await canStartVerification({
-        requestServices: true,
-      });
-      if (isReady) {
-        const result = await startVerification(response);
-        console.log('Started verification for: ' + result);
-      } else {
-        console.log('Not ready');
-      }
-    } catch (error) {
-      console.log(error.code);
-      console.log(error.message);
-    }
-  };
+  // const handleOnSuccess = async (response: any) => {
+  //   setLaunch(false);
+  //   try {
+  //     const isReady = await canStartVerification({
+  //       requestServices: true,
+  //     });
+  //     if (isReady) {
+  //       const result = await startVerification(response);
+  //       console.log('Started verification for: ' + result);
+  //     } else {
+  //       console.log('Not ready');
+  //     }
+  //   } catch (error) {
+  //     console.log(error.code);
+  //     console.log(error.message);
+  //   }
+  // };
 
-  const handleOnError = () => {
-    setLaunch(false);
-  };
+  // const handleOnError = () => {
+  //   setLaunch(false);
+  // };
 
-  const handleOnCloseRequest = () => {
-    setLaunch(false);
-  };
+  // const handleOnCloseRequest = () => {
+  //   setLaunch(false);
+  // };
 
   return (
     <View style={styles.container}>
       <Button onPress={() => setLaunch(true)} title="Start Verification" />
-      <OkHiLocationManager
+      {/* <OkHiLocationManager
         user={{ phone: '+254700110590' }}
         onSuccess={handleOnSuccess}
         onError={handleOnError}
         onCloseRequest={handleOnCloseRequest}
         launch={launch}
-      />
+      /> */}
     </View>
   );
 }
